6ca9b9a9742d5d7e78a1cdbe0a27960f596bbc01
FacuPetry <facundo_petryszak@hotmail.com>
2020-05-28 12:41:48 -0300

    2 files modified

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const app_routes: Routes = [
- { path: 'home', component: HomeComponent}
-
+ { path: 'home', component: HomeComponent},
+ { path: '**', component: HomeComponent}
 ];
 
 export const app_routing = RouterModule.forRoot(app_routes);
