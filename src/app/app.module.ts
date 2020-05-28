1b77e3f11f34b386bf6e5aad2e7efa2a029841d8
FacuPetry <facundo_petryszak@hotmail.com>
2020-05-28 12:30:38 -0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { app_routing } from './app.routes';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, app_routing ],
   declarations: [ AppComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
