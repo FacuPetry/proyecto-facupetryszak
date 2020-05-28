2e8a9b76fb624dfe2835d8f5a4893432bb127948
FacuPetry <facundo_petryszak@hotmail.com>
2020-05-28 12:27:14 -0300

    1 file added

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const app_routes: Routes = [
+ { path: 'home', component: HomeComponent}
+
+];
