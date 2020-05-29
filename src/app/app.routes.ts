8fec35736d1e4e72c97077efd648794c4e65de12
FacuPetry <facundo_petryszak@hotmail.com>
2020-05-29 17:52:58 -0300

    3 files modified

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NosotrosComponent } from './trabaja-nosotros/nosotros.component';
+import { UbicacionComponent } from './ubicacion/ubicacion.component';
 
 const app_routes: Routes = [
  { path: 'home', component: HomeComponent},
  { path: 'nosotros', component: NosotrosComponent},
+ { path: 'ubicacion', component: UbicacionComponent},
  { path: '**', component: HomeComponent}
 ];
 
